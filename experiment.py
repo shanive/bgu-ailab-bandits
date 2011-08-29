95ef5f29097ea917d9c657a26449d3a18d79e6e6
vered shani <shanive21@gmail.com>
2011-08-29 15:02:13 +0300

    made improvements according to issuse

--- experiment.py
+++ experiment.py
@@ -4,7 +4,14 @@ import sys
 import bandit
 import random
 import algorithms
- 
+import logging
+import getopt
+
+
+def usage():
+	"""prints the usage message"""
+ 	print "Usage: python <exp.py> --min <min-pulls> --max <max-pulls> --step <pull-step> --repeat <repetitions> <bandit.txt> <algorithm-name>"
+	
 def myAlgo(resultList):
 	""" receives a lists of arm pulling results and return a number of arm to pull
 	just for testing"""
@@ -32,9 +39,8 @@ def experimentFunc(pullsNum, algorithm, bandit):
 	resultsList = [[] for i in range(bandit.getArmsNum())] # the results of the pulls of every arm.
 	 
 	for pull in range (0, pullsNum):
-		arm = algorithm(resultsList)
 		
-		#print "the chosen arm is: "+str(arm)
+		arm = algorithm(resultsList)
 		
 		pullResult = bandit.pullArm(arm)
 			 
@@ -49,28 +55,22 @@ def experimentFunc(pullsNum, algorithm, bandit):
 def calcAverageRegret(pullsNum, repetitions, algorithm, bandit):
 	""" execute experimentFunc for a given repetitions num and return the average regret"""
 	# initialize sum of the regrets in all repetitions
-	
-	#print "pulls number: " + str(pullsNum)
-	
 	sumOfRegrets = 0.0 	 
 	# execute the experiment Function repetitions times
 	for repeat in range(repetitions):
-		bestArm = experimentFunc(pullsNum, algorithm, bandit)
 		
-		#print "best arm: " + str(bestArm)
+		logging.info('Round number: %d.', repeat)
+		
+		bestArm = experimentFunc(pullsNum, algorithm, bandit)
 		
 		regret = bandit.calcRegret(bestArm)
 		
-		#print "regret: "+str(regret)
+		logging.debug('The best arm and its regret for this round: %d %f respectively.', (bestArm, regret)) 
 		
 		sumOfRegrets += regret
 		
-		#print "sum of regrets: "+str(sumOfRegrets)
-		
 	#calculate average regret
 	avgRegret = float(sumOfRegrets) / repetitions
-	
-	#print "average regret: "+str(avgRegret)
 	
 	return avgRegret
 	 
@@ -87,39 +87,91 @@ def readAvgFromFile(fileName):
 	banditFile = open(fileName, 'r')
 	# string = banditFile.read()
 	avgList = []
+	
+	logging.info('Reading from file.')
+	
 	for line in banditFile:
 		avgList.extend([float(num) for num in line.split()])
 	banditFile.close()
+	
+	logging.info('Done reading from file.')
+	
 	return avgList 
 	  
-def experimentMainLoop(nimPulls,maxPulls, pullStep, repetitions, bandit):
+def experimentMainLoop(nimPulls,maxPulls, pullStep, repetitions, bandit, algorithm):
 	"""the outer loop of the experiment"""
 	pullsNum = minPulls
+	
+	logging.info('Start experiment.') 
+	
 	while pullsNum <= maxPulls:
-		avgRegret = calcAverageRegret(pullsNum, repetitions, algorithms.greedy, bandit)          #### todo ####
+		
+		logging.info('Number of pulls: %f', pullsNum)
+		avgRegret = calcAverageRegret(pullsNum, repetitions, algorithm, bandit)          #### todo ####
 		printResult(pullsNum, avgRegret)
 		
 		pullsNum = pullsNum * pullStep 
-
+		
+	logging.info('Done experimant')
+	
+	
+#def parseCommandLineArg(argv, dict):
+	#"""perse command line arguments"""
+	#try:
+        	#opts, args = getopt.getopt(argv, "", ["min=", "max=", "step=", "repeat="])
+	#except getopt.GetoptError:
+		#usage()
+		#sys.exit(2)
+	#for opt, arg in opts:
+		#if opt == '--min':
+			#dict['minPulls'] = arg
+		#elif opt == 'max':
+			#dict['maxPulls'] = arg
+		#elif opt == '--step':
+			#dict['pullStep'] = arg
+		#elif opt == 'repeat':
+			#dict['repeatitions'] = arg
+	#dict['avgFileName'] = args[0]
+	#dict['algoName'] = args[1]
+		
+			
+	
 if __name__ == "__main__":
 	 # arguments from command-line:
-	if len(sys.argv) < 6:
-		print "Usage: python <exp.py> <bandit.txt> <min-pulls> <max-pulls> <pull-step> <repetitions>"
-		sys.exit(0)
-	avgFileName = sys.argv[1]
-	minPulls =  int(sys.argv[2])
-	maxPulls = int(sys.argv[3])
-	pullStep =  int(sys.argv[4])
-	repetitions = int(sys.argv[5])
+		
+	logging.basicConfig(filename = 'experiment.log', level = logging.INFO)
+	#defult values:	
+	#dict = {'avgFileName':'bandits.txt', 'minPulls':10 , 'maxPulls':10000, 'pullStep': 2, 'repeatitions':1000, 'algoName':'greedy'} 
+	minPulls=10
+	maxPulls=10000
+	pullStep=2
+	repeatitions=1000
+		
+	try:
+        	opts, args = getopt.getopt(sys.argv[1:], "", ["min=", "max=", "step=", "repeat="])
+	except getopt.GetoptError:
+		usage()
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '--min':
+			minPulls = int(arg)
+		elif opt == 'max':
+			maxPulls = int(arg)
+		elif opt == '--step':
+			pullStep = int(arg)
+		elif opt == 'repeat':
+			repeatitions = int(arg)
+			
+	avgFileName = args[0]
+	algoName = args[1]
+	
 	## arguments from file
 	avgList = readAvgFromFile(avgFileName)
 	bandit = bandit.Bandit(avgList)
+	algorithm = getattr(algorithms, algoName) 
 		
 	printFirstLine()
 	
-	experimentMainLoop(minPulls, maxPulls, pullStep, repetitions, bandit)   
+	experimentMainLoop(minPulls, maxPulls, pullStep, repeatitions, bandit, algorithm)
 		
- 
-
-
  
