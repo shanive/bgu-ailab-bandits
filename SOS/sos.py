a2a7ebd6a62c088774b05d3bc55183e832755ae3
David Tolpin <david.tolpin@gmail.com>
2011-09-18 17:20:53 +0300

    cosmetic changes

--- SOS/sos.py
+++ SOS/sos.py
@@ -34,20 +34,25 @@ class Game:
 		
 	def __initValues(self, order):
 		values = range(self.n)
-		if order == 'd':
+		if order == 'u':    # U)p
+			pass # already in accending order
+		elif order == 'd':  # D)own
 			values.reverse()  
-		elif order == 'r':	
+		elif order == 'r':	# R)andom
 			shuffle(values)
-		#else order == 'u'
 		return values
 	
 	def initialState(self):
+		"create an initial state of the game"
 		return State(self.n)
+
+	def __score(self, indices):
+		"return total score of switches at the indices"
+		return sum(self.values[i] for i in indices)
 	
 	def scoreBonus(self, state):
-		return sum(self.values[i] for i in state.whites()) \
-		       - sum(self.values[i] for i in state.blacks())
-
+		"compute game score bonus"
+		return self.__score(state.whites()) - self.__score(state.blacks())
 
 def test_game():
 	game = Game(4, [3, 2, 0, 1])
@@ -58,97 +63,6 @@ def test_game():
 	state.blackMove(3)
 	assert game.scoreBonus(state) == -2
 	
-#class Move:
-	#"""Simulation of a move in SOS game.
-	#Every move has value and color.
-	#A move's color represents the move's state: gray = available, white = choosen by white player, black = choosen by black player"""
-	#def __init__(self, value, color):
-		#self.__value  = value
-		#self.__color = color
-		
-	#def setValue(self, value):
-		#"""changes the move's value"""
-		#self.__value = value 
-	
-	#def setColor(self, color):
-		#"""changes the move's color"""
-		#self.__color = color
-		
-	#def getValue(self):
-		#return self.__value
-	
-	#def getColor(self):
-		#return self.__color
-		
-#class SOS:
-	#"Implementation of Sum Of Switches Game"
-	
-	#def __init__(self, n):
-		#"""Initialize SOS game,
-		#receives the number of possible moves (even number)""" 
-		#self.__n = n
-		
-				## initial values for moves
-		
-		
-	#def getn(self):
-		#"""returns the number of moves"""
-		#return self.__n
-	
-	#def restartGame(self):
-		#"""start a game with the same moves' values"""
-		#for move in self.__moves:
-			#move.setColor('gray')
-		
-			
-	#def newGame(self):
-		#"""starts a new game"""
-		
-		#self.__moves = []
-				
-		#src = [i for i in range(self.__n)]	#moves' values
-		
-		###initialize the moves' values:
-		#for i in range(self.__n):
-			#value = choice(src)		#choose value randomly for move number i
-			#move = Move(value, 'gray')
-			#self.__moves.append(move)
-			#src.remove(value)		#remove the value from values list to prevent duplicate values 
-			
-			
-		
-	#def whiteMove(self, move):
-		#"""add a given move number to white player's moves"""
-		#self.__moves[move].setColor('white')
-		
-	#def blackMove(self, move):
-		#"""add a given move number to black player's moves"""
-		#self.__moves[move].setColor('black')	
-
-	
-	#def endGame(self):
-		#"""return the distance between the white player's score and the black player's score"""
-		
-		#whiteScore = 0
-		#blackScore = 0
-		
-		#for move in self.__moves:
-			#if move.getColor() == 'white':
-				#whiteScore += move.getValue() 	
-			#else:
-				#blackScore += move.getValue()
-				
-		#return whiteScore - blackScore
-	
-	#def availableMoves(self):
-		#"""returns a list of unchoosen moves"""
-		#grayMoves = []
-		#for i in range(self.__n):
-			#if self.__moves[i].getColor() == 'gray':
-				#grayMoves.append(i)
-		#return grayMoves
-
-
 def test_availableMoves():
 	state = State(4)
 	assert state.availableMoves()==[0, 1, 2, 3]
@@ -168,7 +82,7 @@ def test_isFinalState():
 	state.blackMove(3)
 	assert state.isFinalState()
 	
-def test_whites():
+def test_whites_blacks():
 	state = State(4)
 	
 	state.whiteMove(2)
@@ -176,21 +90,12 @@ def test_whites():
 	state.whiteMove(1)
 	state.blackMove(3)
 	assert state.whites()==[1,2]
-	
-def test_blacks():
-	state = State(4)
-	
-	state.whiteMove(2)
-	state.blackMove(0)
-	state.whiteMove(1)
-	state.blackMove(3)
 	assert state.blacks()==[0,3]
 	
 def test_state():
 	test_availableMoves()
 	test_isFinalState()
-	test_whites()
-	test_blacks()
+	test_whites_blacks()
 	
 def test():
 	test_state()
