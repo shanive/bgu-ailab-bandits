1afca90701fa50c7589a43894eec590ca2fce2e8
Vered Shani <shanive21@gmail.com>
2011-11-16 19:11:57 +0200

    implementing SosSgTournament

--- SosSmartGame/SosGame.h
+++ SosSmartGame/SosGame.h
@@ -11,7 +11,7 @@
 #include "SgMove.h"
 /** move's color. */
 typedef enum 
-{
+  {
     /** Available move */
     GREY,
     
@@ -21,183 +21,185 @@ typedef enum
     /** move played by black player */
     BLACK
 
-} MoveColor;
-
-/** switches' values' order */
-typedef enum
-{
-    RANDOM,
-
-    ASCENDING,
-
-    DESCENDING
-
-} ValuesOrder;
-
+  } MoveColor;
 
 
 /**
-Simulation of SOS game's state.
+   Simulation of SOS game's state.
 */
 class SosState
 {
-public:
-	/**
-	constructor.
-	@param size Number of switches.
-	*/
-	SosState(int size);
+ public:
+  
+  
+  /**
+     constructor.
+     @param size Number of switches.
+  */
+  SosState(int size);
 
-	/**
-	copy constructor.
-	@param state The instance to copy.
-	@pre state != 0.
-	*/
- 	SosState(const SosState& state);
+  /**
+     copy constructor.
+     @param state The instance to copy.
+     @pre state != 0.
+  */
+  SosState(const SosState& state);
 
-	/**
-	Destructor.
-	*/
-	~SosState();
+  /**
+     Destructor.
+  */
+  ~SosState();
 	
-	/**
-	@return vector of available moves.
-	*/
-	std::vector<SgMove> availableMoves();
+  /**
+     @return vector of available moves.
+  */
+  std::vector<SgMove> availableMoves();
 
-	/**
-	@return true if it's white's turn.
-	*/
-	bool isWhiteTurn();
+  /**
+     @return true if it's white's turn.
+  */
+  bool isWhiteTurn();
 
-	/**
-	play a move.
-	@param move The move to play.
-	@pre move is in this->avaiableMoves()
-	@post move is not in this->avaiableMoves()
-	@post move is in this->whiteMoves() or in this->blackMoves()
-	(but not in both)
-	*/
-	void play(SgMove move);
+  /**
+     play a move.
+     @param move The move to play.
+     @pre move is in this->avaiableMoves()
+     @post move is not in this->avaiableMoves()
+     @post move is in this->whiteMoves() or in this->blackMoves()
+     (but not in both)
+  */
+  void play(SgMove move);
 	
-	/**
-	@return vector of moves played by white player.
-	*/
-	std::vector<SgMove> whiteMoves();
+  /**
+     @return vector of moves played by white player.
+  */
+  std::vector<SgMove> whiteMoves();
 
-	/**
-	@return vector of moves played by black player.
-	*/
-	std::vector<SgMove> blackMoves();
+  /**
+     @return vector of moves played by black player.
+  */
+  std::vector<SgMove> blackMoves();
 
-	/**
-	@return number of switches.
-	*/
-	int size();
+  /**
+     @return number of switches.
+  */
+  int size();
 
-	/**
-	undo a given number of moves played.
-	@pre this->play has been called n times.
-	@post this->m_played.size() == @pre this->m_played.size() - n
-	*/
-	void undo(int n);
+  /**
+     undo a given number of moves played.
+     @pre this->play has been called n times.
+     @post this->m_played.size() == @pre this->m_played.size() - n
+  */
+  void undo(int n);
 
-private:
-	/*number of switches*/
-	int m_size;
-	/*who's turn is it*/
-	SgBlackWhite m_turn;
-	/*switches*/
-	std::vector<MoveColor> m_moves; 
-	/*played moves. FILO*/
-	std::vector<SgMove> m_played;
-	/*return vector of moves with a givem color*/
-	std::vector<SgMove> someMoves(MoveColor color);
+ private:
+  /*number of switches*/
+  int m_size;
+  /*who's turn is it*/
+  SgBlackWhite m_turn;
+  /*switches*/
+  std::vector<MoveColor> m_moves; 
+  /*played moves. FILO*/
+  std::vector<SgMove> m_played;
+  /*return vector of moves with a givem color*/
+  std::vector<SgMove> someMoves(MoveColor color);
 	
 };
 
 
 class SosGame
 {
-public:
-	/**
-	constructor.
-	*/
-	explicit SosGame(int size, bool scoreBonus = false, ValuesOrder order = RANDOM, std::vector<int>* values = 0);
+ public:
 
-	/**
-	copy constructor.
-	*/
-	SosGame(const SosGame& game);
+  /** switches' values' order */
+  typedef enum
+  {
+    RANDOM,
 
-	/**
-	destructor.
-	*/
-	~SosGame();
+    ASCENDING,
 
-	/**
-	@return true iff a given state is the final state of this game.
-	*/	
-	bool isFinalState(SosState state);
+    DESCENDING
 
-	/**
-	simulate a game between two given players.
-	@param firstplayer A pointer to SosPlayer instance.
-	@param seconfplayer A pointer to SosPlayer instance.
-	@return the score of the game.
-	*/
-	double twoPlayersGame(SosPlayer *firstplayer, SosPlayer *secondplayer);
+  } ValuesOrder;
 
-	/**
-	@param state A final state of this game.
-	@pre isFinalState(state) == true.
-	@return The score of this game.(score bonus or win-loss.)
-	*/ 
-	double gameScore(SosState state);
+  /**
+     constructor.
+  */
+  explicit SosGame(int size, bool scoreBonus = false, ValuesOrder order = RANDOM, std::vector<int>* values = 0);
+
+  /**
+     copy constructor.
+  */
+  SosGame(const SosGame& game);
+
+  /**
+     destructor.
+  */
+  ~SosGame();
+
+  /**
+     @return true iff a given state is the final state of this game.
+  */	
+  bool isFinalState(SosState state);
+
+  /**
+     simulate a game between two given players.
+     @param firstplayer A pointer to SosPlayer instance.
+     @param seconfplayer A pointer to SosPlayer instance.
+     @return the score of the game.
+  */
+  double twoPlayersGame(SosPlayer *firstplayer, SosPlayer *secondplayer);
+
+  /**
+     @param state A final state of this game.
+     @pre isFinalState(state) == true.
+     @return The score of this game.(score bonus or win-loss.)
+  */ 
+  double gameScore(SosState state);
 	
-	/**
-	@param state A state of this game.
-	@return score bonuse of a given state.
-	*/ 
-	double scoreBonus(SosState state);
+  /**
+     @param state A state of this game.
+     @return score bonuse of a given state.
+  */ 
+  double scoreBonus(SosState state);
 	
-	/**
-	@param state A state of this game.
-	@return 1 if white has won and 0 otherwise.
-	*/ 
-	int winLoss(SosState state);
+  /**
+     @param state A state of this game.
+     @return 1 if white has won and 0 otherwise.
+  */ 
+  int winLoss(SosState state);
 
-	/**
-	@return the game's komi (winning value)
-	see Uct article.
-	*/
-	int komi();
+  /**
+     @return the game's komi (winning value)
+     see Uct article.
+  */
+  int komi();
 
-private:
+ private:
 
-	/* The switches' values between 1 to this->m_gameSize */ 
-	std::vector<int> m_switchValues;
+  /* The switches' values between 1 to this->m_gameSize */ 
+  std::vector<int> m_switchValues;
 
-	/* True iff count score bonus. */
-	bool m_scoreBonus;
+  /* True iff count score bonus. */
+  bool m_scoreBonus;
 
-	/* Number of switches */
-	int m_gameSize;
+  /* Number of switches */
+  int m_gameSize;
 
-	/* Initialize switches' values according to order */
-	void initValues(ValuesOrder order);
+  /* Initialize switches' values according to order */
+  void initValues(ValuesOrder order);
 
-	/* return the initial state of this game */
-	SosState initialState();
+  /* return the initial state of this game */
+  SosState initialState();
 
-	/* Return the difference between white's score and black's score */
-	int difference(SosState state);
+  /* Return the difference between white's score and black's score */
+  int difference(SosState state);
 
-	/* receive move index and return it's value */
-	int moveValue(SgMove move);
+  /* receive move index and return it's value */
+  int moveValue(SgMove move);
 
-	/* return the sum of values of moves in moves */
-	int valuesSum(std::vector<SgMove> moves);
+  /* return the sum of values of moves in moves */
+  int valuesSum(std::vector<SgMove> moves);
 
 };
 
--- SosSmartGame/SosSgTournament.cpp
+++ SosSmartGame/SosSgTournament.cpp
@@ -12,33 +12,155 @@ scorebonus/winloss,
 switches order,
 profile
 */
-#include <string.h>
+#include <iostream>
 #include <stdio.h>
-#include <GetOpt.h>
+#include <stdlib.h>
+#include <string>
+#include <stdio.h>
+#include <getopt.h>
 #include <vector>
-using namespace std;
+#include "SosGame.h"
+#include "SosState.h"
+#include "SosPlayer.h"
 
-enum Order{
-  RANDOM,  
-  ASCENDING,
-  DESCENDING
-} ;
+using namespace std;
 
 struct Conf {
   int size;
   vector<string> players;
   int repeat;
   int scorebonus;
-  Order order;
+  SosGame::ValuesOrder order;
   int profile;
-} Conf;
 
-static const struct option longOpts[] = {
-  {"size", required_argument, 0, 0},
+  Conf(): size(10), repeat(1000), scorebonus(0), order(RANDOM), 
+          profile(0)
   {
+  }
+
+  void printer()
+  {
+    cout << "game size: " << this->size <<
+      "\nrepetitions: " << this->repeat <<
+      "\nscorebonus: " << this->scorebonus <<
+      "\norder: " << this->order <<
+      "\nprofile: " << this->profile << endl; 
+    cout << "players:" << endl;
+    vector<string>::iterator it ;
+    for (it = this->players.begin(); it != this->players.end(); ++it)
+      cout << (*it) << " ";
+    cout << endl;
+  }
+};
+
+void usage(){
+  cout << "Usage: /.SosSgTournament.cpp --size <number> --repeat <number> --order <0/1/2> --scorebonus --profile player-name [player-name]..." << endl;
+  exit(2);
+}
+
+/**
+   @param classname the name of the player's type
+   @return an instance of name type
+*/
+SosPlayer* createPlayer(string classname, SosGame *game){
+  if (classname.compare("SosRandomPlayer") == 0)
+    return new SosRandomPlayer(game);
+  if (classname.compare("SosLeftPlayer") == 0)
+    return new SosLeftPlayer(game);
+  if (classname.compare("SosRightPlayer") == 0)
+    return new SosRightPlayer(game);
+}
+
+vector<vector<double>> tournament(Conf *conf)
+{
+  int nuPlayers = conf.players.size();
+  vector<vector<double>> scores;
+
+  //simulates tournament.
+  SosGame game(conf.size, conf.scorebonus, conf.order);
+  
+  vector<string>::iterator fiter;
+  vector<string>::iterator siter;
+  
+  for (fiter = conf.players.begin(); fiter != conf.players.end(); ++fiter)
+    {
+      vector<double> playerscores;
+      for (siter = conf.players.begin() ; siter != conf.players.end(); ++siter)
+        {
+          if (fiter == siter){
+            playerscores.push_back(400.0);
+            continue;
+          }
+          SosPlayer* firstplayer = createPlayer(*fiter, game);
+          SosPlayer* secondplayer = createPlayer(*siter, game);
+          double average = 0.0;
+          for (int i = 0; i < conf.repeat; i++)
+            {
+              average += twoPlayersGame(firstplayer, secondplayer);
+            }
+          playerscores.push_back(average/conf.repeat);
+        }
+      scores.push_back(playerscores);
+    }
+    return scores;
+}
+
 
 
 int main(int argc, char *argv[])
 {
+  Conf conf;
 
-  GetOpt getopt(argc, argv, "   
+  const struct option longOpts[] = {
+  {"size", required_argument, 0, 's'},
+  {"repeat", required_argument, 0, 'r'},
+  {"scorebonus", no_argument, &conf.scorebonus, 1},
+  {"order", required_argument, 0, 'o'},
+  {"profile", no_argument, &conf.profile, 1},
+  {0, 0, 0, 0}
+  };
+
+  //defalt values for conf
+  int option_index = 0;
+  int opt = getopt_long(argc, argv, "", longOpts, &option_index);
+
+  
+  // while not detected the end of the options 
+  while (opt != -1){
+    switch(opt)
+      {
+      case 0:
+        // if this option set a flage do nothing else now 
+        break;
+      case 's':
+        conf.size = atoi(optarg);
+        break;
+      case 'r':
+        conf.repeat = atoi(optarg);
+        break;
+      case 'o':
+        {
+        int order = atoi(optarg);
+        if ((order == RANDOM) || (order == ASCENDING) || (order == DESCENDING))
+          conf.order = static_cast<SosGame::ValuesOrder>(order);
+        else 
+          usage();
+        break;
+        }
+      default:
+        usage();
+      }
+    opt = getopt_long(argc, argv, "", longOpts, &option_index);
+  }
+  // in the variable optind the index in argv of the next remaining argument
+  //unoptional
+  if (optind == argc)
+    usage();
+  else{
+    for (int i = optind; i < argc; i++)
+           conf.players.push_back(argv[i]);
+  }
+
+    conf.printer();
+}     
+
